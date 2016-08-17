d8bce72b86883cb3f2e130613b171168e6342f99
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-08-17 12:40:34 +0100

    Add project files.

---
+++ ContactManager.Database/Contact.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+	[ContactId] INT NOT NULL, 
+    [FirstName] NVARCHAR(255) NULL, 
+    [LastName] NVARCHAR(255) NOT NULL,
+
+	CONSTRAINT PK_Contact PRIMARY KEY NONCLUSTERED (ContactID),
+	INDEX IX_Contact_LastName (LastName)
+)

536bce90f9caa25ee4962eee2d1bb2e560c6dfa9
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-08-19 14:30:34 +0100

    add api project with hello world endpoint

---
+++ ContactManager.API/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ContactManager.API {
+
+    /// <summary>
+    /// main owin startup class
+    /// </summary>
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+
+            // use attribute routing
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);  
+                 
+        }
+
+    }
+}

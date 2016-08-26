dfeb3b50025fa5149942249827650f76a95713ee
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-08-26 21:57:07 +0100

    add api tests where api is self hosted

---
+++ ContactManager.APITests/ContactAPITests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Owin.Hosting;
+using ContactManager.API;
+using System.Net.Http;
+
+namespace ContactManager.APITests {
+
+    [TestClass]
+    public class ContactAPITests {
+
+        [TestMethod]
+        public void Starter() {
+
+            string baseAddress = "http://localhost:9000/";
+
+            using (WebApp.Start<Startup>(baseAddress)) {
+
+                var client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/hello").Result;
+
+                var result = response.Content.ReadAsStringAsync().Result;
+
+                Assert.AreEqual("\"hello world\"", result);
+
+            }
+
+        }
+
+    }
+
+}
